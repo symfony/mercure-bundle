55591e98e0a95277530a124e6cbd484048aa8631
Kévin Dunglas <dunglas@gmail.com>
2021-06-25 17:48:50 +0200

    feat: add Twig helper generating hub URLs and setting authorization cookies

--- src/DependencyInjection/MercureExtension.php
+++ src/DependencyInjection/MercureExtension.php
@@ -27,6 +27,7 @@ use Symfony\Component\Mercure\Authorization;
 use Symfony\Component\Mercure\Debug\TraceableHub;
 use Symfony\Component\Mercure\Debug\TraceablePublisher;
 use Symfony\Component\Mercure\Discovery;
+use Symfony\Component\Mercure\EventSubscriber\SetCookieSubscriber;
 use Symfony\Component\Mercure\Hub;
 use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\Mercure\HubRegistry;
@@ -40,9 +41,11 @@ use Symfony\Component\Mercure\Jwt\TokenProviderInterface;
 use Symfony\Component\Mercure\Messenger\UpdateHandler;
 use Symfony\Component\Mercure\Publisher;
 use Symfony\Component\Mercure\PublisherInterface;
+use Symfony\Component\Mercure\Twig\MercureExtension as TwigMercureExtension;
 use Symfony\Component\Stopwatch\Stopwatch;
 use Symfony\UX\Turbo\Bridge\Mercure\Broadcaster;
 use Symfony\UX\Turbo\Bridge\Mercure\TurboStreamListenRenderer;
+use Twig\Environment;
 
 /**
  * @author Kévin Dunglas <dunglas@gmail.com>
@@ -255,9 +258,20 @@ final class MercureExtension extends Extension
             ->addArgument($config['default_cookie_lifetime'])
         ;
 
-        $container->register(Discovery::class, Discovery::class)
+        $container->register(Discovery::class)
             ->addArgument(new Reference(HubRegistry::class))
         ;
+
+        if (class_exists(SetCookieSubscriber::class)) {
+            $container->register(SetCookieSubscriber::class)
+                ->addTag('kernel.event_subscriber', ['priority' => -10]);
+        }
+
+        if (class_exists(TwigMercureExtension::class) && class_exists(Environment::class)) {
+            $container->register(TwigMercureExtension::class)
+                ->setArguments([new Reference(HubRegistry::class), new Reference(Authorization::class), new Reference('request_stack')])
+                ->addTag('twig.extension');
+        }
 
         // TODO: remove these parameters in the next release.
         $container->setParameter('mercure.hubs', $hubUrls);
