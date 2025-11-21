7ab58f12bb3693ab7af281cda22f7cc52a3bcbe3
Kévin Dunglas <kevin@dunglas.fr>
2025-11-21 11:37:27 +0100

    test version

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -15,6 +15,7 @@ namespace Symfony\Bundle\MercureBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Mercure\FrankenPhpHub;
 
 /**
  * MercureExtension configuration structure.
@@ -25,7 +26,7 @@ final class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $builtinPublish = \function_exists('mercure_publish');
+        $builtinPublish = class_exists(FrankenPhpHub::class) && \function_exists('mercure_publish');
 
         $treeBuilder = new TreeBuilder('mercure');
         $rootNode = $treeBuilder->getRootNode();
